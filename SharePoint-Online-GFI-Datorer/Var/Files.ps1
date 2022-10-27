d045070ed7ab11ee08c2cfb9683a3206b57e446a
Kristjan Sigurdsson <krsigurdsson@gmail.com>
2022-10-27 16:17:19 +0200

    Add files via upload

---
+++ SharePoint-Online-GFI-Datorer/Var/Files.ps1
@@ -0,0 +1,13 @@
+#Files
+$fileFolder = "c:\scriptfolder"
+$spamlist = "spamboxar.txt"
+$fileExtension = ".txt"
+
+#misc
+$rows = 0
+
+#Lists
+$listName = "SnowFlakes"
+
+#Create new list
+$listDescription= "OMG"  #"<List Description>"
